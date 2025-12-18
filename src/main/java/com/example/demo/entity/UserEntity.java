ad086a953a784643af9d69cd29c6be09205110ba
bala-amypo <bala@amypo.in>
2025-12-18 09:14:34 +0000

    User commit

--- src/main/java/com/example/demo/entity/UserEntity.java
+++ src/main/java/com/example/demo/entity/UserEntity.java
@@ -1,61 +1,27 @@
-package com.example.demo.entity;
+package com.example.demo.model;
 
+import jakarta.persistence.*;
+import lombok.*;
 
+import java.util.Set;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "users")
 public class UserEntity {
-    Long id;
-    String name;
-    String email;
-    String password;
-    String role;
 
-    public UserEntity(Long id,String name,String email,String password,String role){
-        this.id=id;
-        this.name=name;
-        this.email=email;
-        this.password=password;
-        this.role=role;
-    }
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-    public UserEntity() {
-    }
+    @Column(unique = true)
+    private String email;
 
-    public Long getId() {
-        return id;
-    }
+    private String password;
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getRole() {
-        return role;
-    }
-
-    public void setRole(String role) {
-        this.role = role;
-    }
+    @ElementCollection(fetch = FetchType.EAGER)
+    private Set<String> role;
 }
