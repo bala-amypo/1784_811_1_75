b2afb30ae6e92620329866a74fc063b88931e35a
bala-amypo <bala@amypo.in>
2025-12-19 06:03:21 +0000

    User commit

--- src/main/java/com/example/demo/entity/RiskScoreEntity.java
+++ src/main/java/com/example/demo/entity/RiskScoreEntity.java
@@ -10,7 +10,7 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class RiskScoreEntity {
+public class RiskScoreEntity {          
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
