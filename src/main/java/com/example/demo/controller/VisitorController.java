d179eb310230e478ec1de8b25e611c68eccc067d
bala-amypo <bala@amypo.in>
2025-12-20 12:48:09 +0000

    User commit

--- src/main/java/com/example/demo/controller/VisitorController.java
+++ src/main/java/com/example/demo/controller/VisitorController.java
@@ -1,3 +1,11 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.Visitor;
+import com.example.demo.service.VisitorService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
 @RestController
 @RequestMapping("/visitors")
 public class VisitorController {
@@ -8,16 +16,25 @@ public class VisitorController {
         this.service = service;
     }
 
+    // CREATE
     @PostMapping
     public Visitor create(@RequestBody Visitor visitor) {
         return service.save(visitor);
     }
 
+    // READ
     @GetMapping
     public List<Visitor> getAll() {
-        return service.getAll();
+        return service.findAll();
     }
 
+    // UPDATE
+    @PostMapping("/update")
+    public Visitor update(@RequestBody Visitor visitor) {
+        return service.update(visitor);
+    }
+
+    // DELETE
     @DeleteMapping("/{id}")
     public void delete(@PathVariable Long id) {
         service.delete(id);
