6499b49b29bcb38bcec06214afdb3783c7de883f
bala-amypo <bala@amypo.in>
2025-12-21 08:33:37 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/impl/VisitLogServiceImpl.java
@@ -0,0 +1,33 @@
+package com.example.demo.service.impl;
+
+import com.example.demo.model.VisitLog;
+import com.example.demo.repository.VisitLogRepository;
+import com.example.demo.service.VisitLogService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class VisitLogServiceImpl implements VisitLogService {
+
+    private final VisitLogRepository visitLogRepository;
+
+    public VisitLogServiceImpl(VisitLogRepository visitLogRepository) {
+        this.visitLogRepository = visitLogRepository;
+    }
+
+    @Override
+    public VisitLog save(VisitLog visitLog) {
+        return visitLogRepository.save(visitLog);
+    }
+
+    @Override
+    public List<VisitLog> findAll() {
+        return visitLogRepository.findAll();
+    }
+
+    @Override
+    public VisitLog findById(Long id) {
+        return visitLogRepository.findById(id).orElse(null);
+    }
+}
