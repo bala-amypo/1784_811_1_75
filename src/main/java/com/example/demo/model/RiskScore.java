838a25f5ad874cd968300f726e5fbd3187619e83
bala-amypo <bala@amypo.in>
2025-12-21 09:21:09 +0000

    User commit

---
+++ src/main/java/com/example/demo/model/RiskScore.java
@@ -0,0 +1,25 @@
+package com.example.demo.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class RiskScore {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "visitor_id")
+    private Visitor visitor;
+
+    private Integer totalScore;
+    private String riskLevel;
+    private LocalDateTime evaluatedAt;
+}
