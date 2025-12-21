a04d21e53658f560933a89dfe1a9c0598867743b
bala-amypo <bala@amypo.in>
2025-12-21 08:32:44 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/VisitLogService.java
@@ -0,0 +1,14 @@
+package com.example.demo.service;
+
+import com.example.demo.model.VisitLog;
+
+import java.util.List;
+
+public interface VisitLogService {
+
+    VisitLog save(VisitLog visitLog);
+
+    List<VisitLog> findAll();
+
+    VisitLog findById(Long id);
+}
