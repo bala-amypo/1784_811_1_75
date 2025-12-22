fb2205504a161270464872a38f33171a8b77a079
bala-amypo <bala@amypo.in>
2025-12-22 03:56:34 +0000

    User commit

--- src/main/java/com/example/demo/repository/ScoreAuditLogRepository.java
+++ src/main/java/com/example/demo/repository/ScoreAuditLogRepository.java
@@ -7,6 +7,6 @@ import java.util.List;
 
 public interface ScoreAuditLogRepository extends JpaRepository<ScoreAuditLog, Long> {
 
-    // required by problem statement
+   
     List<ScoreAuditLog> findByVisitorId(Long visitorId);
 }
