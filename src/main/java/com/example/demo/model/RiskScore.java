ea0cc7cefefbd63f11c1bbd92b68a91e5f4ce759
bala-amypo <bala@amypo.in>
2025-12-26 14:00:24 +0000

    User commit

--- src/main/java/com/example/demo/model/RiskScore.java
+++ src/main/java/com/example/demo/model/RiskScore.java
@@ -3,32 +3,23 @@ package com.example.demo.model;
 import jakarta.persistence.*;
 import lombok.*;
 
-import java.time.LocalDateTime;
-
 @Entity
 @Getter
 @Setter
-@Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class RiskScore {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @OneToOne
-    @JoinColumn(name = "visitor_id")
-    private Visitor visitor;
-
     private Integer totalScore;
 
     private String riskLevel;
 
-    private LocalDateTime evaluatedAt;
-
-    @PrePersist
-    public void prePersist() {
-        this.evaluatedAt = LocalDateTime.now();
-    }
+    @OneToOne
+    @JoinColumn(name = "visitor_id")
+    private Visitor visitor;
 }
