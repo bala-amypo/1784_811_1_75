e4a38bdf02f64e86866c1fdbedf10c47974f55f8
bala-amypo <bala@amypo.in>
2025-12-26 08:26:22 +0000

    User commit

--- src/main/java/com/example/demo/dto/AuthRequest.java
+++ src/main/java/com/example/demo/dto/AuthRequest.java
@@ -1,14 +1,11 @@
 package com.example.demo.dto;
 
-import lombok.*;
+import lombok.Getter;
+import lombok.Setter;
 
 @Getter
 @Setter
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
 public class AuthRequest {
-
     private String email;
     private String password;
 }
