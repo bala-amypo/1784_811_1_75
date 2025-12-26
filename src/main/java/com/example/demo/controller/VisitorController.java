c9e46bdac78e15f95ff2a16de061b39b200d2d50
bala-amypo <bala@amypo.in>
2025-12-26 06:53:34 +0000

    User commit

--- src/main/java/com/example/demo/controller/VisitorController.java
+++ src/main/java/com/example/demo/controller/VisitorController.java
@@ -2,41 +2,35 @@ package com.example.demo.controller;
 
 import com.example.demo.model.Visitor;
 import com.example.demo.service.VisitorService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/visitors")
+@RequiredArgsConstructor
 public class VisitorController {
 
     private final VisitorService visitorService;
 
-    public VisitorController(VisitorService visitorService) {
-        this.visitorService = visitorService;
+    @PostMapping
+    public ResponseEntity<Visitor> create(
+            @RequestBody Visitor visitor
+    ) {
+        return ResponseEntity.ok(visitorService.createVisitor(visitor));
     }
 
-   
-    @PostMapping("/save")
-    public Visitor saveVisitor(@RequestBody Visitor visitor) {
-        return visitorService.saveVisitor(visitor); 
-    }
-
-  
-    @GetMapping("/all")
-    public List<Visitor> getAllVisitors() {
-        return visitorService.getAllVisitors(); 
-    }
-
-  
     @GetMapping("/{id}")
-    public Visitor getVisitorById(@PathVariable Long id) {
-        return visitorService.getVisitorById(id); 
+    public ResponseEntity<Visitor> get(
+            @PathVariable Long id
+    ) {
+        return ResponseEntity.ok(visitorService.getVisitor(id));
     }
 
-    
-    @DeleteMapping("/{id}")
-    public void deleteVisitor(@PathVariable Long id) {
-        visitorService.deleteVisitor(id); 
+    @GetMapping
+    public ResponseEntity<List<Visitor>> getAll() {
+        return ResponseEntity.ok(visitorService.getAllVisitors());
     }
 }
