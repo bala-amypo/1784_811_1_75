dba1d5157d9ed5a7eb41bbc593f0396bfe39e9b0
bala-amypo <bala@amypo.in>
2025-12-26 06:54:06 +0000

    User commit

--- src/main/java/com/example/demo/controller/RiskRuleController.java
+++ src/main/java/com/example/demo/controller/RiskRuleController.java
@@ -2,32 +2,35 @@ package com.example.demo.controller;
 
 import com.example.demo.model.RiskRule;
 import com.example.demo.service.RiskRuleService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/riskrules")
+@RequestMapping("/risk-rules")
+@RequiredArgsConstructor
 public class RiskRuleController {
 
     private final RiskRuleService riskRuleService;
 
-    public RiskRuleController(RiskRuleService riskRuleService) {
-        this.riskRuleService = riskRuleService;
-    }
-
     @PostMapping
-    public RiskRule create(@RequestBody RiskRule riskRule) {
-        return riskRuleService.save(riskRule);
+    public ResponseEntity<RiskRule> create(
+            @RequestBody RiskRule rule
+    ) {
+        return ResponseEntity.ok(riskRuleService.createRule(rule));
     }
 
     @GetMapping
-    public List<RiskRule> getAll() {
-        return riskRuleService.findAll();
+    public ResponseEntity<List<RiskRule>> getAll() {
+        return ResponseEntity.ok(riskRuleService.getAllRules());
     }
 
     @GetMapping("/{id}")
-    public RiskRule getById(@PathVariable Long id) {
-        return riskRuleService.findById(id);
+    public ResponseEntity<RiskRule> get(
+            @PathVariable Long id
+    ) {
+        return ResponseEntity.ok(riskRuleService.getRule(id));
     }
 }
