f1c6a691e06695311a9c2fa8191501e331f8a1ac
bala-amypo <bala@amypo.in>
2025-12-26 14:05:39 +0000

    User commit

--- src/main/java/com/example/demo/service/RiskRuleService.java
+++ src/main/java/com/example/demo/service/RiskRuleService.java
@@ -1,13 +1,14 @@
 package com.example.demo.service;
 
 import com.example.demo.model.RiskRule;
+
 import java.util.List;
 
 public interface RiskRuleService {
 
     RiskRule createRule(RiskRule rule);
 
-    List<RiskRule> getAllRules();
-
     RiskRule getRule(Long id);
+
+    List<RiskRule> getAllRules();
 }
