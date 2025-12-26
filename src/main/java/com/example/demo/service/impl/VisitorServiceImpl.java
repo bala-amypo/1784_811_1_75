f74a394dc5fa6596f13567041ba2ba0524b8d953
bala-amypo <bala@amypo.in>
2025-12-26 14:07:13 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/VisitorServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/VisitorServiceImpl.java
@@ -1,31 +1,31 @@
 package com.example.demo.service.impl;
 
+import com.example.demo.exception.ResourceNotFoundException;
 import com.example.demo.model.Visitor;
 import com.example.demo.repository.VisitorRepository;
 import com.example.demo.service.VisitorService;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
-@RequiredArgsConstructor
 public class VisitorServiceImpl implements VisitorService {
 
     private final VisitorRepository visitorRepository;
 
+    public VisitorServiceImpl(VisitorRepository visitorRepository) {
+        this.visitorRepository = visitorRepository;
+    }
+
     @Override
     public Visitor createVisitor(Visitor visitor) {
-        if (visitor.getPhone() == null || visitor.getPhone().isBlank()) {
-            throw new IllegalArgumentException("phone required");
-        }
         return visitorRepository.save(visitor);
     }
 
     @Override
     public Visitor getVisitor(Long id) {
         return visitorRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("not found"));
+                .orElseThrow(() -> new ResourceNotFoundException("Visitor not found"));
     }
 
     @Override
