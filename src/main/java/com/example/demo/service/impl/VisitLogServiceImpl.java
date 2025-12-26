e2c6fc6d4b23e8330cc5e90aed83949e6104a2f1
bala-amypo <bala@amypo.in>
2025-12-26 06:47:09 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/VisitLogServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/VisitLogServiceImpl.java
@@ -1,32 +1,46 @@
 package com.example.demo.service.impl;
 
 import com.example.demo.model.VisitLog;
+import com.example.demo.model.Visitor;
 import com.example.demo.repository.VisitLogRepository;
+import com.example.demo.repository.VisitorRepository;
 import com.example.demo.service.VisitLogService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class VisitLogServiceImpl implements VisitLogService {
 
     private final VisitLogRepository visitLogRepository;
+    private final VisitorRepository visitorRepository;
 
-    public VisitLogServiceImpl(VisitLogRepository visitLogRepository) {
-        this.visitLogRepository = visitLogRepository;
+    @Override
+    public VisitLog createVisitLog(Long visitorId, VisitLog log) {
+
+        Visitor visitor = visitorRepository.findById(visitorId)
+                .orElseThrow(() -> new RuntimeException("not found"));
+
+        if (log.getExitTime() != null &&
+            log.getEntryTime() != null &&
+            log.getExitTime().isBefore(log.getEntryTime())) {
+            throw new IllegalArgumentException("exitTime must be after entryTime");
+        }
+
+        log.setVisitor(visitor);
+        return visitLogRepository.save(log);
     }
 
     @Override
-    public VisitLog save(VisitLog visitLog) {
-        return visitLogRepository.save(visitLog);
+    public VisitLog getLog(Long id) {
+        return visitLogRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("not found"));
     }
 
     @Override
-    public List<VisitLog> findAll() {
+    public List<VisitLog> getLogsByVisitor(Long visitorId) {
         return visitLogRepository.findAll();
-    }
-
-    @Override
-    public VisitLog findById(Long id) {
-        return visitLogRepository.findById(id).orElse(null);
     }
 }
