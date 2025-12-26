a21d4dbe0adc7c4e8320d799cd6b6acbd0d04ac3
bala-amypo <bala@amypo.in>
2025-12-26 06:47:34 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/RiskScoreServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/RiskScoreServiceImpl.java
@@ -1,33 +1,48 @@
 package com.example.demo.service.impl;
 
 import com.example.demo.model.RiskScore;
+import com.example.demo.model.Visitor;
 import com.example.demo.repository.RiskScoreRepository;
+import com.example.demo.repository.VisitorRepository;
 import com.example.demo.service.RiskScoreService;
+import com.example.demo.util.RiskLevelUtils;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class RiskScoreServiceImpl implements RiskScoreService {
 
     private final RiskScoreRepository riskScoreRepository;
-
-    public RiskScoreServiceImpl(RiskScoreRepository riskScoreRepository) {
-        this.riskScoreRepository = riskScoreRepository;
-    }
+    private final VisitorRepository visitorRepository;
 
     @Override
-    public RiskScore save(RiskScore riskScore) {
+    public RiskScore evaluateVisitor(Long visitorId) {
+
+        Visitor visitor = visitorRepository.findById(visitorId)
+                .orElseThrow(() -> new RuntimeException("not found"));
+
+        int score = 0;
+
+        RiskScore riskScore = RiskScore.builder()
+                .visitor(visitor)
+                .totalScore(score)
+                .riskLevel(RiskLevelUtils.determineRiskLevel(score))
+                .build();
+
         return riskScoreRepository.save(riskScore);
     }
 
     @Override
-    public List<RiskScore> findAll() {
-        return riskScoreRepository.findAll();
+    public RiskScore getScoreForVisitor(Long visitorId) {
+        return riskScoreRepository.findByVisitorId(visitorId)
+                .orElseThrow(() -> new RuntimeException("not found"));
     }
 
     @Override
-    public RiskScore findById(Long id) {
-        return riskScoreRepository.findById(id).orElse(null);
+    public List<RiskScore> getAllScores() {
+        return riskScoreRepository.findAll();
     }
 }
