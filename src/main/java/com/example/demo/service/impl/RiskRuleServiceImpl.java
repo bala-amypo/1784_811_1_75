56e30e129185b50a62655acbbf9f2df6e5e6d3aa
bala-amypo <bala@amypo.in>
2025-12-26 14:07:33 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/RiskRuleServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/RiskRuleServiceImpl.java
@@ -1,41 +1,35 @@
 package com.example.demo.service.impl;
 
+import com.example.demo.exception.ResourceNotFoundException;
 import com.example.demo.model.RiskRule;
 import com.example.demo.repository.RiskRuleRepository;
 import com.example.demo.service.RiskRuleService;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
-@RequiredArgsConstructor
 public class RiskRuleServiceImpl implements RiskRuleService {
 
     private final RiskRuleRepository riskRuleRepository;
 
-    @Override
-    public RiskRule createRule(RiskRule rule) {
-
-        if (riskRuleRepository.findByRuleName(rule.getRuleName()).isPresent()) {
-            throw new RuntimeException("Rule name must be unique");
-        }
-
-        if (rule.getThreshold() < 0 || rule.getScoreImpact() < 0) {
-            throw new IllegalArgumentException("Invalid values");
-        }
-
-        return riskRuleRepository.save(rule);
+    public RiskRuleServiceImpl(RiskRuleRepository riskRuleRepository) {
+        this.riskRuleRepository = riskRuleRepository;
     }
 
     @Override
-    public List<RiskRule> getAllRules() {
-        return riskRuleRepository.findAll();
+    public RiskRule createRule(RiskRule rule) {
+        return riskRuleRepository.save(rule);
     }
 
     @Override
     public RiskRule getRule(Long id) {
         return riskRuleRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("not found"));
+                .orElseThrow(() -> new ResourceNotFoundException("RiskRule not found"));
+    }
+
+    @Override
+    public List<RiskRule> getAllRules() {
+        return riskRuleRepository.findAll();
     }
 }
